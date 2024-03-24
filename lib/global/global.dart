aec4ddee03efa32952a91508e41abcf753a2772b
LeonardoDuarteCoelho <leonardo04duarte@gmail.com>
2024-03-24 23:56:00 +0000

    App can now redirect the user to the PortuGO's official website.

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -15,7 +15,7 @@ DriverModel driverData = DriverModel();
 String? driverVehicleType = '';
 int maxNumberOfRatingStarts = 5;
 // TODO: Replace this placeholder URL, which is the one for the Figma project, with the real URL for the website:
-const portuGoWebsiteUrl = 'https://www.figma.com/file/RoybjZ7YVGofmuPIOL3yZi/PortuGO-Layouts?type=design&node-id=0-1&mode=design&t=gbdp1JyJlpgoAWWy-0';
+const portuGoWebsiteUrl = 'https://portugo-c7f05.web.app/';
 String driverRatingDescription = '';
 double driverRatingsNumber = 0;
 int numberOfTripsToBeDisplayedInHistory = 0;
